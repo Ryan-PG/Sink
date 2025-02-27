6858f3174be5495fdfaadc502c2098abf408fd9c
Ryan Heida <heidarih313@gmail.com>
2025-02-27 19:19:16 +0330

    Fixed header picture size and HERO picture

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,31 +1,28 @@
 <script setup>
-import heroImg from '@/public/IMG_4146-01_NEW.png'
+import heroImg from '@/assets/images/IMG_4146-01_NEW.png'
 // import { AreaChart } from 'lucide-vue-next'
 // import { GitHubIcon } from 'vue3-simple-icons'
 
-const { title, description } = useAppConfig()
+const {
+  description,
+} = useAppConfig()
 </script>
 
 <template>
-  <main
-    class="grid pt-8 pb-8 lg:grid-cols-2 place-items-center md:py-12"
-  >
+  <main class="grid pt-8 pb-8 lg:grid-cols-2 place-items-center md:py-12">
     <div>
-      <h1
-        class="text-5xl font-bold lg:text-6xl xl:text-7xl lg:tracking-tight xl:tracking-tighter"
-      >
-        {{ title }}
+      <h1 class="text-5xl font-bold lg:text-6xl xl:text-7xl lg:tracking-tight xl:tracking-tighter">
+        <!-- {{ title }} -->
+        Ryan Heida<br>
+        Link Shortener
       </h1>
       <p class="max-w-xl mt-4 text-lg text-slate-600">
         {{ description }}
       </p>
       <div class="flex flex-col gap-3 mt-6 sm:flex-row">
         <HomeLink
-          href="https://links.ryanheida.com/"
-          target="_blank"
-          title="Ryan Heida Links"
-          class="flex items-center justify-center gap-1"
-          rel="noopener"
+          href="https://links.ryanheida.com/" target="_blank" title="Ryan Heida Links"
+          class="flex items-center justify-center gap-1" rel="noopener"
         >
           <!-- <AreaChart
             class="w-5 h-5"
@@ -49,10 +46,7 @@ const { title, description } = useAppConfig()
       </div>
     </div>
     <div class="hidden py-6 md:block">
-      <div
-        class="w-[512px]"
-        v-html="heroImg"
-      />
+      <div class="w-[512px]" v-html="heroImg" />
     </div>
   </main>
 </template>
